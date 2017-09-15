c7c24981ef5834ece867bc35f832bd8ecd169ee8
smartin <smartin@SMARTIN-PC>
2017-09-15 17:26:36 -0400

    Add project files.

---
+++ Sample Calculator/Controllers/CalculatorController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sample_Calculator.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        // GET: Calculator
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        
+    }
+}
